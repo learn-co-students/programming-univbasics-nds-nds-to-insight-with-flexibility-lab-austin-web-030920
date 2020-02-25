788e89c29b80b867308f7dc08c937aaa27b6f432
l.jackson96 <l.jackson96@outlook.com>
2020-02-25 09:53:23 -0600

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,5 +1,6 @@
 # Provided, don't edit
 require 'directors_database'
+require 'pry'
 
 # A method we're giving you. This "flattens"  Arrays of Arrays so: [[1,2],
 # [3,4,5], [6]] => [1,2,3,4,5,6].
@@ -34,6 +35,7 @@ end
 # Your code after this point
 
 def movies_with_director_key(name, movies_collection)
+  #binding.pry
   # GOAL: For each Hash in an Array (movies_collection), provide a collection
   # of movies and a directors name to the movie_with_director_name method
   # and accumulate the returned Array of movies into a new Array that's
@@ -48,6 +50,17 @@ def movies_with_director_key(name, movies_collection)
   # Array of Hashes where each Hash represents a movie; however, they should all have a
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
+
+  result = []
+
+  i = 0
+  while i < movies_collection.length do
+    #binding.pry
+    movie_data = movies_collection[i]
+   result << movie_with_director_name(name, movie_data)
+   i += 1
+  end
+  result
 end
 
 
@@ -63,6 +76,19 @@ def gross_per_studio(collection)
   #
   # Hash whose keys are the studio names and whose values are the sum
   # total of all the worldwide_gross numbers for every movie in the input Hash
+
+  result = {}
+
+  i = 0
+  while i < collection.length do 
+    binding.pry
+    data = collection[i]
+    result[data] += data[:worldwide_gross]
+
+
+
+  end
+  result
 end
 
 def movies_with_directors_set(source)
